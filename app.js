7bfb08c3e8d76e86a6a6218f52479249fb65f743
Julien Auger <jauger@amiltone.com>
2023-03-06 15:49:11 +0100

    Adding router

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-
+const fs = require('fs')
+const url = require('url')
 const http = require('http');
 const { logger } = require('./modules/logger');
 
@@ -8,10 +8,42 @@ require('dotenv').config()
 const port = process.env.PORT || 3000
 
 const server = http.createServer(async (req, res) => {  
-  logger(req)
+  const { pathname } = url.parse(req.url)
 
-  res.statusCode = 200
-  res.end('Hello world')
+  const page = pathname
+  
+  logger(req)
+  console.log(page)
+
+  fs.readdir('./pages', (err, pages) => {
+    if (err) throw err;
+    
+    if (req.url === '/') {
+      res.statusCode = 200
+      res.setHeader('content-type', 'text/html')
+
+      fs.readFile('./pages/home.html', (err, data) => {
+        if (err) throw err;
+        res.end(data)
+      })
+    }
+    
+    const currentPage = pages.filter((page) => page.split('.')[0] === req.url.slice(1))
+
+    if (currentPage) {
+      res.statusCode = 200
+      res.setHeader('content-type', 'text/html')
+
+      fs.readFile(`./pages/${currentPage}`, (err, data) => {
+        res.end(data)
+      })
+    }
+
+    res.statusCode = 404
+  })
+
+ 
+
 })
 
 server.listen(port, () => {
